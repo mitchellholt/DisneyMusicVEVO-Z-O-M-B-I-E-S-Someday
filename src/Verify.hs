a2c92af355ad9e4e27ed3968caea8aceab29a3ad
Joel Richardson <joelwrichardson01@gmail.com>
2022-08-27 15:50:50 +1000

    Verify done (hopefully) [needs some integration tho]

--- src/Verify.hs
+++ src/Verify.hs
@@ -15,6 +15,11 @@ top (Case (s,_) _ _ _ _) = s
 top (Apply (s,_) _ _) = s
 top (Truth (s,_)) = s
 
+grabRules :: (Pf a) -> [Rules a]
+grabRules (Expand (_,r) _ _) = r
+grabRules (Case (_,r) _ _ _ _) = r
+grabRules (Apply (_,r) _ _) = r
+grabRules (Truth (_,r)) = r
 
 split :: (Stmt a) -> (Stmt a -> Stmt a, Stmt a)
 split (Or a b) = (\x -> (Or x b), a)
@@ -29,3 +34,16 @@ verifyS (Apply (s,_) r p) = ((top p) == (r s)) & (verifyS p)
 verifyS (Case (s,_) r t f n) = and $ (fmap verifyS [t, f, n]) ++ (fmap (\x -> s == (top x)) [t, f, n]) ++ [(bot t) == (top n), (bot f) == (top n)]
 verifyS (Expand (s,_) p n) = let (f, ss) = (split s) in 
     ((top p) == ss) & ((f $ bot p) == (top n)) & (verifyS n) & (verifyS p)
+
+-- awful
+subList :: [a] -> [a] -> Bool
+subList a b = and $ fmap (\x -> elem x b) a
+
+verifyR :: (Pf a) -> Bool
+verifyR (Truth _) -> True
+verifyR (Apply (_,rr) r p) = (elem r rr) & (subList (grabRules p) rr)
+verifyR (Expand (_,rr) p n) = (subList (grabRules p) rr) & (subList (grabRules n) rr)
+verifyR (Case (_,rr) r t f n) = (subList (grabRules t) (r:rr)) & (subList (grabRules f) ((not r):rr)) & (subList (grabRules f) rr)
+
+verify :: (Pf a) -> Bool
+verify p = (verifyS p) & (verifyR p)
